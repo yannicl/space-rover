f138ed7eb3018468a1a4ee933c857f01c93088f8
Yannic Luyckx <yannic.luyckx@gmail.com>
2024-03-25 21:33:21 -0400

    improve keyboard logic

--- app/test-keyboard.py
+++ app/test-keyboard.py
@@ -9,49 +9,8 @@ FORWARD = 100
 REVERSE = -100
 STOP = 0
 
-motorA = STOP
-motorB = STOP
-
-def on_press(key):
-    global motorA, motorB
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-        if key.char == 'a' :
-            motorA = FORWARD
-            motorB = REVERSE
-        if key.char == 'd' :
-            motorA = REVERSE
-            motorB = FORWARD
-        if key.char == 'w' :
-            motorA = FORWARD
-            motorB = FORWARD
-        if key.char == 's' :
-            motorA = REVERSE
-            motorB = REVERSE
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
-
-def on_release(key):
-    global motorA, motorB
-    print('{0} released'.format(
-        key))
-    motorA = STOP
-    motorB = STOP
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
-
-while True:
-    line = ser.readline()
-    print(line)
+def sendCommands(motorA, motorB):
+    global ser
     msg = {
         "type":"RC_CHANNELS_SCALED",
         "chan1_scaled":motorA,
@@ -62,8 +21,50 @@ while True:
     print(cmd)
     ser.write(cmd.encode("utf-8"))
     ser.write( b'\x04' )
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+        if key.char == 'd' :
+            motorA = FORWARD
+            motorB = REVERSE
+            sendCommands(motorA, motorB)
+        if key.char == 'a' :
+            motorA = REVERSE
+            motorB = FORWARD
+            sendCommands(motorA, motorB)
+        if key.char == 'w' :
+            motorA = FORWARD
+            motorB = FORWARD
+            sendCommands(motorA, motorB)
+        if key.char == 's' :
+            motorA = REVERSE
+            motorB = REVERSE
+            sendCommands(motorA, motorB)
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    motorA = STOP
+    motorB = STOP
+    sendCommands(motorA, motorB)
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+sendCommands(STOP, STOP)
+
+while True:
     line = ser.readline()
     print(line)
-    line = ser.readline()
-    print(line)
-    time.sleep(0.5)
+    time.sleep(0.1)
